09b3ef6cb71739b375a59eb3dbdf8ca1e9841cec
saiba <saiba@balaji>
2021-04-24 17:14:09 +0530

    24/04/2021

---
+++ Spring-MVC-CRUD/src/main/java/com/Spring/model/Employee.java
@@ -0,0 +1,37 @@
+package com.Spring.model;
+
+public class Employee {    
+private int id;    
+private String name;    
+private float salary;    
+private String designation;  
+
+public Employee() {
+	super();
+}
+public int getId() {    
+    return id;    
+}    
+public void setId(int id) {    
+    this.id = id;    
+}    
+public String getName() {    
+    return name;    
+}    
+public void setName(String name) {    
+    this.name = name;    
+}    
+public float getSalary() {    
+    return salary;    
+}    
+public void setSalary(float salary) {    
+    this.salary = salary;    
+}    
+public String getDesignation() {    
+    return designation;    
+}    
+public void setDesignation(String designation) {    
+    this.designation = designation;    
+}    
+    
+}    
